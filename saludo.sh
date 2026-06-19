8715df16af37dfed75c0ca14e35405b2ee87a289
KevinFernandez21 <136862908+KevinFernandez21@users.noreply.github.com>
2026-06-18 22:38:57 -0500

    fourth commit

--- saludo.sh
+++ saludo.sh
@@ -1,3 +1,4 @@
 echo "Hola Mundo desde jenkins bykdfernan"
 echo "Probando triggers desde git github y jenkins"
 echo "Kevin se equivoco de nuevo"
+echo "probando triger con webhook desde git y usando grok ;)" 
